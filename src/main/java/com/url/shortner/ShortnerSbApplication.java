01e02c49a1057d1fa014d0d2398079c3af3211ad
Arbab Ahmed <arbabahmed631@gmail.com>
2026-03-27 21:50:15 +0530

    Added CROS Functionality

--- src/main/java/com/url/shortner/ShortnerSbApplication.java
+++ src/main/java/com/url/shortner/ShortnerSbApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShortnerSbApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(ShortnerSbApplication.class, args);
     }
 
