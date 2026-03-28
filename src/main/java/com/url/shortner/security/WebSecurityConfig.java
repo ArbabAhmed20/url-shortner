1c1868facbcc6855c07426ff59c1a6637fc53fca
Arbab Ahmed <arbabahmed631@gmail.com>
2026-03-28 21:31:34 +0530

    Fixed CORS policy related to React Query in FrontEnd

--- src/main/java/com/url/shortner/security/WebSecurityConfig.java
+++ src/main/java/com/url/shortner/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.url.shortner.service.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -51,6 +52,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf.disable().authorizeHttpRequests(auth ->
                 auth.requestMatchers("/api/auth/**").permitAll()
+//                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/urls/**").authenticated()
                         .requestMatchers("/{shortUrl}").permitAll()
                         .anyRequest().authenticated()
